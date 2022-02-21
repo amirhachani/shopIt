2a7cb64a60fd277336cfaffe7ae1f49d1cb05992
Amir Hachani <hachaniami@gmail.com>
2022-02-21 09:53:24 +0000

    authorize_roles_done

--- backEnd/middlewares/auth.js
+++ backEnd/middlewares/auth.js
@@ -3,13 +3,14 @@ const catchAsyncErrors = require('./catchAsyncErrors')
 const jwt = require('jsonwebtoken')
 const ErrorHandler = require('../utils/errorHandler')
 const User = require('../models/user')
+const req = require('express/lib/request')
 // Checks if user is authenticated or not.
 
 
 exports.isAuthenticatedUser = catchAsyncErrors( async (req, res, next) => {
 
     const { token } = req.cookies
-
+ 
     if(!token){
         return next(new ErrorHandler('Please login to be able to access .', 401))
     }
@@ -19,3 +20,15 @@ exports.isAuthenticatedUser = catchAsyncErrors( async (req, res, next) => {
 
     next()
 })
+
+// Handling users Roles
+
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            return next(
+            new ErrorHandler(`Role (${req.user.role}) is not allowed to access this resource`, 403)
+            )}
+        next()
+    }
+}
