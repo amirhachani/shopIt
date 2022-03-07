cefbc697996f1914ec128e369ed448627fec50d9
Amir Hachani <hachaniami@gmail.com>
2022-03-07 09:40:50 +0100

    resetPasswordToken_updated

--- backEnd/models/user.js
+++ backEnd/models/user.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs")
 const mongoose = require("mongoose")
 const validator = require("validator")
 const jwt = require("jsonwebtoken")
+const crypto  = require('crypto')
 
 
 const userSchema = new mongoose.Schema({
@@ -69,5 +70,20 @@ userSchema.pre('save', async function (next){
 
     this.password = await bcrypt.hash(this.password, 10)
 }) 
+
+// Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+    // Generate token from
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash and set to resetPasswordToken
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+    // Set token expire time 
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000
+
+    return resetToken
+}
+ 
 
 module.exports = mongoose.model('User', userSchema)
